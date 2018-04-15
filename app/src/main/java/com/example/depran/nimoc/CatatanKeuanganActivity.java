1911553ed1aa8dcc0870e60636e582221bc9f9bf
amalia.susianti <Amalia.susianti@mail.unpas.ac.id>
2018-04-15 12:20:14 +0700

    progress amal

---
+++ app/src/main/java/com/example/depran/nimoc/CatatanKeuanganActivity.java
@@ -0,0 +1,13 @@
+package com.example.depran.nimoc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CatatanKeuanganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catatan_keuangan);
+    }
+}
