033eb0c95f14a58abf00cba1760e9df6bd5169de
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2018-04-22 18:40:06 +0700

    merge

--- app/src/main/java/com/example/depran/nimoc/buku/BerandaActivity.java
+++ app/src/main/java/com/example/depran/nimoc/buku/BerandaActivity.java
@@ -2,17 +2,23 @@ package com.example.depran.nimoc.buku;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.depran.nimoc.R;
 
-public class BerandaActivity extends AppCompatActivity {
+public class BerandaActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         findViewById(R.id.btnTambah).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +27,10 @@ public class BerandaActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
